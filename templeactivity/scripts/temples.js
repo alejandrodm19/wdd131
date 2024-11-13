5040ee60988eeeef18821d88965f4033ad1fb25e
Alejandro Malvacias <alejandromalvacias1@gmail.com>
2024-11-12 20:20:30 -0400

    End of the activity

---
+++ templeactivity/scripts/temples.js
@@ -0,0 +1,16 @@
+
+const yearSpan = document.getElementById("year");
+const lastModifiedSpan = document.getElementById("lastModified");
+yearSpan.textContent = new Date().getFullYear();
+lastModifiedSpan.textContent = document.lastModified;
+
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("open");
+  hamburger.textContent = menu.classList.contains("open") ? "✖" : "☰";
+});
+
+menu.classList.add("open");
